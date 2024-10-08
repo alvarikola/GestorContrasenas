0ed9d8218e221ab4fecac9cb45d385e4ce69aa81
alvarikola <alvaroe484@gmail.com>
2024-10-08 13:35:00 +0100

    pruebas

--- app/src/main/java/com/example/gestorcontraseas/MainActivity.kt
+++ app/src/main/java/com/example/gestorcontraseas/MainActivity.kt
@@ -41,7 +41,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Contenedor(modifier: Modifier = Modifier) {
     var usuario:String = "Alvaro"
-    var contrasena:String = "4444"
+    var contrasena:String = "1111"
     Text(
         text = "Gestor de Contraseñas",
         fontSize = 30.sp,
