946730b4b6c4586f938f9f3f0f628f6dff23059d
Yoel <yobelb8@gmail.com>
2024-10-14 12:48:00 +0100

    Ocultar contraseña update

--- app/src/main/java/com/example/gestorcontraseas/MainActivity.kt
+++ app/src/main/java/com/example/gestorcontraseas/MainActivity.kt
@@ -103,7 +103,7 @@ fun Contenedor(nombreArchivo: String) {
                     trailingIcon = {
                         // Icono para mostrar/ocultar la contraseña
                         IconButton(onClick = { isPasswordVisible = !isPasswordVisible }) {
-                            Text(if (isPasswordVisible) "Ocultar" else "Mostrar") // Cambia el texto del botón
+                            Text(if (isPasswordVisible) "◠" else "⚆") // Cambia el texto del botón
                         }
                     },
                 )
